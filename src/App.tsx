d8a9ca43d79163ca2f8b9ab025d9defb7e8ed241
umeshcb <umeshbadiger395@gmail.com>
2023-09-06 17:07:11 +0530

    display genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react'
-import NavBar from './components/NavBar'
-import GameGrid from './components/GameGrid'
-import GenreList from './components/GenreList'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <Grid templateAreas={{
-    base: `"nav" "main"`,
-    lg: `"nav   nav" "aside main"`
-  }}>
-    <GridItem area='nav'>
-      <NavBar />
-    </GridItem>
-    <Show above='lg'>
-      <GridItem area='aside' >
-        <GenreList />
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav   nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
-    </Show>
-    <GridItem area='main' >
-      <GameGrid />
-    </GridItem>
-  </Grid>
+      <Show above="lg">
+        <GridItem area="aside" paddingX={"5px"}>
+          <GenreList />
+        </GridItem>
+      </Show>
+      <GridItem area="main">
+        <GameGrid />
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
