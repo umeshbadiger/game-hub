d0aebd92eb72d5191e11b6925fb6c3ca01c10cd8
umeshcb <umeshbadiger395@gmail.com>
2023-09-07 22:33:34 +0530

    sorting the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,11 +1,9 @@
 import { SimpleGrid } from "@chakra-ui/react";
+import { GameQuery } from "../App";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
-import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../hooks/useGenres";
-import { Platform } from "../hooks/usePlatforms";
-import { GameQuery } from "../App";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 interface Props {
     gameQuery: GameQuery;
